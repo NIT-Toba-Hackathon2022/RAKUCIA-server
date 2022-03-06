477098412c4377d06adfa5f0adf61ed352952c9a
Re-taro <aminokacho@gmail.com>
2022-03-06 23:06:22 +0900

    🐛 fix url

--- src/linebot/linebot.service.ts
+++ src/linebot/linebot.service.ts
@@ -265,10 +265,78 @@ export class LinebotService {
 
   async setRichMenu(): Promise<void> {
     const client = new Client(this.linebotConfigService.createLinebotOptions());
-    const richMenuId = await client.createRichMenu(richMenu);
+    const richMenuId = await client.createRichMenu({
+      size: {
+        width: 1200,
+        height: 405,
+      },
+      selected: true,
+      name: 'リッチメニュー',
+      chatBarText: 'メニュー一覧',
+      areas: [
+        {
+          bounds: {
+            x: 0,
+            y: 0,
+            width: 600,
+            height: 405,
+          },
+          action: {
+            type: 'uri',
+            uri: this.configService.get<string>('LIFF_URL'),
+          },
+        },
+        {
+          bounds: {
+            x: 600,
+            y: 0,
+            width: 600,
+            height: 405,
+          },
+          action: {
+            type: 'postback',
+            data: 'choose',
+          },
+        },
+      ],
+    });
     await client.setRichMenuImage(richMenuId, fs.createReadStream(join(process.cwd(), 'src/linebot/richmenu.png')));
     await client.setDefaultRichMenu(richMenuId);
-    await client.createRichMenu(richMenu);
+    await client.createRichMenu({
+      size: {
+        width: 1200,
+        height: 405,
+      },
+      selected: true,
+      name: 'リッチメニュー',
+      chatBarText: 'メニュー一覧',
+      areas: [
+        {
+          bounds: {
+            x: 0,
+            y: 0,
+            width: 600,
+            height: 405,
+          },
+          action: {
+            type: 'uri',
+            uri: this.configService.get<string>('LIFF_URL'),
+          },
+        },
+        {
+          bounds: {
+            x: 600,
+            y: 0,
+            width: 600,
+            height: 405,
+          },
+          action: {
+            type: 'postback',
+            data: 'choose',
+          },
+        },
+      ],
+    });
   }
 
   createFoodMessage(items: FoodCreateInput[], id: string): FlexMessage {
